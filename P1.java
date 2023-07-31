0b288d79cd254af7986edc79a3ef1e7e1fc0c68d
Vaishnavi Sanjay Padhar <padharvaishnavi02@gmail.com>
2023-07-31 23:32:52 +0530

    P1 arrays

--- P1.java
+++ P1.java
@@ -1,3 +1,27 @@
 // 1)Create an Array of size 10 of integers. 
 // Take input from the user for these 10 elements and print the entire array after that.
 
+import java.util.Scanner;
+
+public class P1{
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 10 integers:");
+
+        // Taking input for each element of the array
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        // Printing the entire array
+        System.out.println("Array elements:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        // Don't forget to close the scanner
+        scanner.close();
+    }
+}
