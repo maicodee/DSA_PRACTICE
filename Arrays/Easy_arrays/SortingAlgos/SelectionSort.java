5a2d7c7951a4eb306824e3dbca0c3e6652ab2817
Vaishnavi Sanjay Padhar <padharvaishnavi02@gmail.com>
2023-09-06 20:50:15 +0530

    new codes

--- Arrays/Easy_arrays/SortingAlgos/SelectionSort.java
+++ Arrays/Easy_arrays/SortingAlgos/SelectionSort.java
@@ -2,11 +2,42 @@ package SortingAlgos;
 import java.util.Scanner;
 public class SelectionSort {
 
-public static void main(String args[]){
+    public static void selectionsort(int arr[] ,int n){
 
-Scanner sc = new Scanner(System.in);
+        for(int i=0; i<n; i++){
+            int smallest = i;
+            for(int j=i+1;j<n;j++){
 
-sc.close();
-}
+                if(arr[smallest]>arr[j]){
+                    smallest = j;
+                }
+            }
 
+            int temp = arr[i];
+            arr[i] = arr[smallest];
+            arr[smallest] = temp;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int k = sc.nextInt();
+        int arr[] = new int[k];
+        for(int i =0;i<k;i++){
+
+            arr[i] = sc.nextInt();
+
+        }
+        
+        int n = arr.length;
+        sc.close();
+        selectionsort(arr,n);
+
+        for(int j =0;j<k;j++){
+            System.out.print(arr[j] + " ");
+        }
+    }
 }
