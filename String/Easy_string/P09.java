5731635d0b26c2dd368b7dd79fc7487b6e18959e
Vaishnavi Sanjay Padhar <padharvaishnavi02@gmail.com>
2023-10-14 22:18:05 +0530

    new

--- String/Easy_string/P09.java
+++ String/Easy_string/P09.java
@@ -3,8 +3,23 @@
 package String.Easy_string;
 
 import java.util.Scanner;
+// import java.util.Arrays;
 
 public class P09 {
-    Scanner sc = new Scanner(System.in);
-    String str1 = sc.nextLine();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        System.out.println(str1);
+        sc.close();
+        char[] arr1 = str1.toCharArray();
+        int n = arr1.length;
+
+        //This below code will reverse each chacter of the line 
+        // for (int i = n-1; i > 0; i--) {
+        //     System.out.print(arr1[i]);
+        // }
+
+        //This below code will reverse each "word" of the line
+    }
+
 }
